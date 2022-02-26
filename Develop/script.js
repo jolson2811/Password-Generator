d6493ea6cb50e62d734093d5114aa2279d344674
jolson2811 <jolson2811@gmail.com>
2022-02-25 18:48:43 -0700

    wrote out pseudo code based on acceptance criteria

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,15 @@
 // Assignment code here
-
-
+// Declare variables that will store input from user
+function generatePassword() {
+  //First Prompt - Get Length - min 8, max 128 characters
+  // Must answer at least one prompt from prompts 2-5
+  // Second Prompt - Include Lowercase
+  // Third Prompt - Include Uppercase
+  // Fourth Prompt - Include Numeric
+  // Fifth Prompt - Include Special Characters
+  // Create random password based on criteria
+  // Return created password
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
